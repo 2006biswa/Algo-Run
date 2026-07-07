7706f34d2437d503f00e8dba072bcb7698145698
Biswa <sbiswabhusan13@gmail.com>
2026-07-07 12:23:54 +0530

    Update API URL for production deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust in production
+    baseURL: 'https://algo-run.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
